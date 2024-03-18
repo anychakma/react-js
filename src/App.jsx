0d561c5c642744fd05d3605ad2201eca2058bb58
Coderany <anychakmawebdesigner@gmail.com>
2024-03-18 21:09:58 +0600

    how to work virtual dom

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ export function App(){
+
+
+    // let text = "Hi"
+     let x = 5;
+    // if(x<10){
+    //     text = "Hello"
+    // }
+
+return (
+    <>
+    <h1> {x <10 ? "Hi" : "Hello"}</h1>
+    </>
+);
+}
